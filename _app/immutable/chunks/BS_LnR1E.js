03accf70cae526f60014ad3ea67f1bfe1f523fd2
Kol Crooks <kolcrooks10@gmail.com>
2025-01-27 00:23:58 -0500

    Updates

---
+++ _app/immutable/chunks/BS_LnR1E.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1kyi7g8)==null?void 0:s.base)??"/kol";var a;const t=((a=globalThis.__sveltekit_1kyi7g8)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/entry/start.DMeZjD7p.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/C3Owl7WM.js";export{t as start};
