e90add83466e7ea0934a985c53c2edc40639b265
Kol Crooks <kolcrooks10@gmail.com>
2025-02-27 08:01:56 -0500

    fix formatting

--- src/lib/blog/quoteblock.svelte
+++ src/lib/blog/quoteblock.svelte
@@ -1,3 +1,3 @@
-<blockquote class="p-4 my-4 border-s-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800">
+<blockquote class="p-4 my-4 border-s-4 border-gray-300 bg-gray-50 ">
     <slot />
 </blockquote>
